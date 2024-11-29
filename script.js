2d804aef271464092e37d550683fb3e460258586
ninjakiller-bit <jr66981@gmail.com>
2024-11-28 23:44:45 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,20 @@
     // First hide all content regions, then show the content-region specified in the URL hash 
     // (or if no hash URL is found, default to first menu item)
     $('.content-region').hide();
-    
+
+    const form = document.getElementById('contact-form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    fetch('https://formspree.io/f/your-formspree-id', {
+        method: 'POST',
+        body: formData,
+    })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error));
+});    
     // Remove any active classes on the main-menu
     $('.main-menu a').removeClass('active');
     var region = location.hash.toString() || $('.main-menu a:first').attr('href');
